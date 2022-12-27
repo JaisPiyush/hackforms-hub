686a89dee1d507855f8a832877ce364df6b22cc4
Piyush Jaiswal <46999854+JaisPiyush@users.noreply.github.com>
2022-12-28 03:19:01 +0530

    Web3StorageDelegate along with tests completed

--- src/storage/web3_storage.spec.ts
+++ src/storage/web3_storage.spec.ts
@@ -1,6 +1,67 @@
+import { Web3StorageBasedError } from "./errors";
+import {File} from 'web3.storage';
+import { Web3StorageDelegate, makeFileObjectFromString } from "./web3_storage";
+import * as dotenv from 'dotenv';
 
+jest.setTimeout(10000)
 describe('Testing WEB3STORAGE utility module', () => {
-    test('should throw error due to no API Key', () => { 
-        
+    beforeEach(() => {
+        dotenv.config();
+    })
+
+    test('should throw error due to no API Token', () => { 
+        expect(process.env.WEB3STORAGE_TOKEN).toBeDefined();
+        delete process.env.WEB3STORAGE_TOKEN
+        expect(process.env.WEB3STORAGE_TOKEN).toBeUndefined();
+        try {
+            const client = new Web3StorageDelegate();
+            throw new Error('delegate creation must throw error due to no API Token')
+        }catch (e) {
+            expect(e).toBeInstanceOf(Web3StorageBasedError);
+        }
+    });
+
+    test('should create storage client', () => {
+        try {
+            
+            expect(process.env.WEB3STORAGE_TOKEN).toBeDefined();
+            const client = new Web3StorageDelegate();
+            expect(client).toBeInstanceOf(Web3StorageDelegate);
+        }catch(e) {
+            throw new Error(`Should not throw error, has thrown: (${e})`);
+        }
+    });
+
+    test('testing makeFileObjectFromString', () => {
+        const file = makeFileObjectFromString(JSON.stringify('rerrange'), 'rearrage.txt');
+        expect(file).toBeInstanceOf(File);
+    });
+
+    test('testing store for json data', async () => {
+        const client = new Web3StorageDelegate();
+        const data = JSON.stringify({name: 'Piyush'});
+        const cid = await client.store(data, client.generateRandomName());
+        expect(cid).toBeTruthy();
+    })
+
+    test('testing status for json data', async () => {
+        const client = new Web3StorageDelegate();
+        const obj = {name: "Piyush"};
+        const data  = JSON.stringify(obj);
+        const cid = await client.store(data, client.generateRandomName());
+        const status = await client.getStatus(cid);
+        expect(status).toBeTruthy();
+    });
+
+    test('testing get for json data', async () => {
+        jest.setTimeout(20000)
+        const client = new Web3StorageDelegate();
+        const obj = {name: "Piyush"};
+        const data  = JSON.stringify(obj);
+        const cid = await client.store(data, client.generateRandomName());
+        const file = await client.get(cid);
+        console.log(file);
+        expect(JSON.parse(await file.text())).toStrictEqual(obj);
+
     })
 });
--- src/storage/web3_storage.ts
+++ src/storage/web3_storage.ts
@@ -1,5 +1,6 @@
-import { Web3Storage, PutOptions } from "web3.storage";
+import { Web3Storage, PutOptions, File } from "web3.storage";
 import { Web3StorageBasedError } from "./errors";
+import { v4 as uuidv4 } from 'uuid';
 
 function getAccessToken() {
     return process.env.WEB3STORAGE_TOKEN;
@@ -14,9 +15,14 @@ function makeStorageClient(token?: string): Web3Storage {
     
 }
 
-export function makeFileObjectFromString(data: string, name: string, options?: FilePropertyBag): File {
-    const blob = new Blob([data], {type: 'application/json'});
-    return new File([blob], name, options);
+export function makeFileObjectFromString(data: string, name: string, 
+    options?: FilePropertyBag): File {
+    const buffer  = Buffer.from(data);
+    if (options === undefined) {
+        options = {};
+    }
+    options.type = options.type || 'application/json';
+    return new File([buffer], name, options);
 }
 
 
@@ -29,17 +35,14 @@ export class Web3StorageDelegate {
         this.storageClient = makeStorageClient(token);
     }
 
-    public async store(data: string, name: string, 
-        options?: {fileOptions?: FilePropertyBag, storageOptions?: PutOptions}) {
+    public async storeFile(file: any, storageOptions?: PutOptions) {
         try {
-            const file = makeFileObjectFromString(data, name, options?.fileOptions);
             // Storage options are used to alter the behavior of @method put in the storage client
             // by default web3.storage creates a directory to store files within it for utilizing human namings
             // For our use case it's useless since directories are immutable too grouping of data won't do any good to us
-            const storageOptions = options?.storageOptions || {};
+            storageOptions = storageOptions || {};
             storageOptions.wrapWithDirectory = storageOptions.wrapWithDirectory ||  false;
             storageOptions.maxRetries = storageOptions.maxRetries || this.MAX_RETRIES;
-
             // put method is expected to return cid string
             return await this.storageClient.put([file], storageOptions);
         }catch (e) {
@@ -47,12 +50,35 @@ export class Web3StorageDelegate {
         }
     }
 
+    public async store(data: string, name: string, 
+        options?: {fileOptions?: FilePropertyBag, storageOptions?: PutOptions}) {
+        const file = makeFileObjectFromString(data, name, options?.fileOptions);      
+        return await this.storeFile(file, options?.storageOptions);
+        
+    }
+
     public async get(cid: string) {
-        return await this.storageClient.get(cid);
+        const res =  await this.storageClient.get(cid);
+        const files = await res?.files();
+        if (files === undefined) {
+            throw new Web3StorageBasedError('file retrieval failed')
+        }
+        return files[0];
     }
 
     public async getStatus(cid: string): Promise<boolean> {
         const status = await this.storageClient.status(cid);
         return status !== undefined && status.cid !== undefined;
     }
+
+    public getUrl(cid: string): string {
+        return `https://${cid}.ipfs.w3s.link/`;
+    }
+
+    public generateRandomName(): string {
+        return uuidv4();
+    }
+
+    
+
 }
